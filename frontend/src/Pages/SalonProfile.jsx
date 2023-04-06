43358b0454c1b8e6f939d8ce139f2547b9c3f5b6
menuka <madurangamenuka@gamil.com>
2023-04-07 02:10:56 +0530

    salon profile

--- frontend/src/Pages/SalonProfile.jsx
+++ frontend/src/Pages/SalonProfile.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import ProfileS from "./ProfileS";
+import AuthenticationServices from "../Services/AuthenticationServices";
 
 const SalonProfile = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [user, SetUser] = useState();
+  const [salonId, setSalonId] = useState("");
+  const [salonDetials, setSalonDetails] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const dataFromLocalStorage = localStorage.getItem("loggedUser");
+    const parseData = JSON.parse(dataFromLocalStorage);
+    const userIdFromData = parseData?.userId;
+    setSalonId(userIdFromData);
+    console.log(salonId);
+  }, []);
+
+  useEffect(() => {
+    async function fetchSalonDetails() {
+      setLoading(true);
+      try {
+        const response = await AuthenticationServices.GetSalonDetails(salonId);
+        if (response.data.code == "00") {
+          setSalonDetails(response.data.content);
+          console.log(response.data.content);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    if (salonId) {
+      fetchSalonDetails();
+    }
+  }, [salonId]);
   return (
-    <div>
+    <div className="bg-white">
       <Header />
-      <h1>Hello Salon</h1>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : salonDetials ? (
+          <h1 className="flex items-center justify-center text-5xl font-bold text-blue-600 md:text-6xl">
+            Hello {salonDetials.name}
+          </h1>
+        ) : (
+          <p>No salon details found.</p>
+        )}
+      </div>
+      <Footer />
     </div>
   );
 };
